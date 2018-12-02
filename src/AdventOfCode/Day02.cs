e653b02df9b172bb97795e0bed5918c6b8c7c7e3
Adam Rodger <adam.rodger@gmail.com>
2018-12-02 07:47:06 +0000

    Day 2

---
+++ src/AdventOfCode/Day02.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 2
+    /// </summary>
+    public class Day02
+    {
+        private const string InputFile = "inputs/day02.txt";
+
+        public int Part1()
+        {
+            var lines = File.ReadAllLines(InputFile);
+
+            int doubles = 0;
+            int triples = 0;
+
+            foreach (string line in lines)
+            {
+                var groups = line.GroupBy(c => c).ToArray();
+
+                if (groups.Any(g => g.Count() == 2))
+                {
+                    doubles++;
+                }
+                if (groups.Any(g => g.Count() == 3))
+                {
+                    triples++;
+                }
+            }
+
+            return doubles * triples;
+        }
+
+        public string Part2()
+        {
+            var lines = File.ReadAllLines(InputFile);
+
+            foreach (string outer in lines)
+            {
+                foreach (string inner in lines)
+                {
+                    int diff = 0;
+                    int diffIndex = 0;
+
+                    for (int i = 0; i < outer.Length; i++)
+                    {
+                        if(outer[i] != inner[i])
+                        {
+                            diff++;
+                            diffIndex = i;
+                        }
+                    }
+
+                    if (diff == 1)
+                    {
+                        return outer.Remove(diffIndex, 1);
+                    }
+                }
+            }
+
+            return "Not found";
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day02Tests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day02Tests
+    {
+        [Fact]
+        public void Part1_WhenCalled_ProvidesSolution()
+        {
+            var solve = new Day02();
+
+            int actual = solve.Part1();
+
+            Assert.Equal(5928, actual);
+        }
+
+        [Fact]
+        public void Part2_WhenCalled_ProvidesSolution()
+        {
+            var solve = new Day02();
+
+            string actual = solve.Part2();
+
+            Assert.Equal("bqlporuexkwzyabnmgjqctvfs", actual);
+        }
+    }
+}
