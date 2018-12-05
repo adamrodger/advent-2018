fd104859e42c146bf7a60e0894a881c4feadb021
Adam Rodger <adam.rodger@gmail.com>
2018-12-05 20:36:28 +0000

    Added some templates to make starting a new day faster

--- tests/AdventOfCode.Tests/Day01Tests.cs
+++ tests/AdventOfCode.Tests/Day01Tests.cs
@@ -23,21 +23,5 @@ namespace AdventOfCode.Tests
 
             Assert.Equal(655, actual);
         }
-
-        /*[Theory]
-        [InlineData("1212", 6)]
-        [InlineData("1221", 0)]
-        [InlineData("123425", 4)]
-        [InlineData("123123", 12)]
-        [InlineData("12131415", 4)]
-        [InlineData(Part2Input, 1092)]
-        public void Part2_WhenCalled_ProvidesSolution(string input, int expected)
-        {
-            var solve = new Day01();
-
-            int actual = solve.Part2(input);
-
-            Assert.Equal(expected, actual);
-        }*/
     }
 }
---
+++ tests/AdventOfCode.Tests/DayXXTests.cs
@@ -0,0 +1,78 @@
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    using TestAttribute = System.Runtime.CompilerServices.CompilerGeneratedAttribute;
+    //using TestAttribute = Xunit.FactAttribute; // uncomment to enable tests
+
+    // use ctrl+h to replace all XX with day number
+    public class DayXXTests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly DayXX solver;
+
+        public DayXXTests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new DayXX();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/dayXX.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                
+            };
+        }
+
+        [Test]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Test]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day XX - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Test]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Test]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day XX - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
