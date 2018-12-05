aa8410a7462ad4e0ba9cc1db37f60f2efd1b12be
Adam Rodger <adam.rodger@gmail.com>
2018-12-05 07:39:57 +0000

    Day 5

---
+++ src/AdventOfCode/Day05.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 5
+    /// </summary>
+    public class Day05
+    {
+
+        public int React(string input)
+        {
+            bool found = false;
+
+            do
+            {
+                found = false;
+
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    if (Math.Abs(input[i] - input[i + 1]) == 32)
+                    {
+                        input = input.Remove(i, 2);
+                        found = true;
+                        i--;
+                    }
+                }
+            } while (found);
+
+            return input.Length;
+        }
+
+        public int Part2(string input)
+        {
+            int shortest = int.MaxValue;
+
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                string replaced = input.Replace(c.ToString(), string.Empty).Replace(c.ToString().ToUpper(), string.Empty);
+                int length = this.React(replaced);
+
+                if (length < shortest)
+                {
+                    shortest = length;
+                }
+            }
+
+            return shortest;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day05Tests.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day05Tests
+    {
+        [Fact]
+        public void Part1_SampleInput_ProvidesSolution()
+        {
+            string input = "dabAcCaCBAcCcaDA";
+            var solve = new Day05();
+
+            int actual = solve.React(input);
+
+            Assert.Equal(10, actual);
+        }
+
+        [Fact]
+        public void Part1_WhenCalled_ProvidesSolution()
+        {
+            var input = File.ReadAllText("inputs/day05.txt");
+            var solve = new Day05();
+
+            int actual = solve.React(input);
+
+            Assert.Equal(11590, actual);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProvidesSolution()
+        {
+            string input = "dabAcCaCBAcCcaDA";
+            var solve = new Day05();
+
+            int actual = solve.Part2(input);
+
+            Assert.Equal(4, actual);
+        }
+
+        [Fact]
+        public void Part2_WhenCalled_ProvidesSolution()
+        {
+            var input = File.ReadAllText("inputs/day05.txt");
+            var solve = new Day05();
+
+            int actual = solve.Part2(input);
+
+            Assert.Equal(4504, actual);
+        }
+    }
+}
