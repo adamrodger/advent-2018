c1b796b514d5f0d38c3e94a3c56aaa3d8d9aa701
Adam Rodger <adam.rodger@gmail.com>
2018-12-08 07:24:11 +0000

    Day 8 - tidied up

--- src/AdventOfCode/Day08.cs
+++ src/AdventOfCode/Day08.cs
@@ -28,68 +28,79 @@ namespace AdventOfCode
         {
             int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
             int index = 0;
-            (int part1, int part2) = SumMetadata(numbers, null, ref index);
+            (int part1, int part2) = this.SumMetadata(numbers, null, ref index);
 
             return (part1, part2);
         }
 
         public (int, int) SumMetadata(int[] input, Node parent, ref int offset)
         {
-            var node = new Node
-            {
-                Id = NextId,
-                Children = input[offset++],
-                MetadataSize = input[offset++]
-            };
+            var node = new Node { Id = NextId++ };
 
+            // root node has no parent
             if (parent != null)
             {
                 parent.ChildNodes.Add(node);
             }
 
-            NextId = (char)((int)NextId + 1);
+            // get the header values and advance the offset
+            int children = input[offset++];
+            int metadata = input[offset++];
 
-            for (int i = 0; i <  node.Children; i++)
+            // recurse into children starting at the new offset
+            for (int i = 0; i < children; i++)
             {
-                // recurse into children
                 node.MetadataSum += this.SumMetadata(input, node, ref offset).Item1;
             }
 
-            for (int i = offset; i < offset + node.MetadataSize; i++)
+            // add up metadata from the offset
+            for (int i = offset; i < offset + metadata; i++)
             {
                 node.MetadataValues.Add(input[i]);
                 node.MetadataSum += input[i];
             }
 
-            offset += node.MetadataSize;
+            // skip the offset over the metadata to the next node
+            offset += node.MetadataValues.Count;
 
             int childTotal = 0;
 
+            // we've reached the end, calc the root node
             if (offset == input.Length)
             {
-                CalculateTreeSum(node, ref childTotal);
+                this.CalculateTreeSum(node, ref childTotal);
             }
 
             return (node.MetadataSum, childTotal);
         }
 
+        /// <summary>
+        /// Use the metadata to sum child node values recursively
+        ///
+        /// Leaf nodes are the sum of their metadata values
+        /// Branch nodes are the sum of their child nodes at the indices of their metadata (if they exist)
+        /// </summary>
         private void CalculateTreeSum(Node node, ref int total)
         {
-            // we've reached the end, calc the root node
             foreach (int childIndex in node.MetadataValues)
             {
-                if (childIndex > 0 && node.ChildNodes.Count > 0 && childIndex <= node.ChildNodes.Count)
+                if (!node.ChildNodes.Any() || childIndex > node.ChildNodes.Count)
                 {
-                    var childNode = node.ChildNodes.ElementAt(childIndex - 1);
+                    continue;
+                }
 
-                    if (childNode.Children > 0)
-                    {
-                        CalculateTreeSum(childNode, ref total);
-                    }
-                    else
-                    {
-                        total += childNode.MetadataValues.Sum();
-                    }
+                // child index is 1-based, not 0-based
+                var childNode = node.ChildNodes.ElementAt(childIndex - 1);
+
+                if (childNode.ChildNodes.Any())
+                {
+                    // recurse
+                    this.CalculateTreeSum(childNode, ref total);
+                }
+                else
+                {
+                    // leaf nodes are the sum of their metadata
+                    total += childNode.MetadataValues.Sum();
                 }
             }
         }
@@ -98,11 +109,7 @@ namespace AdventOfCode
         {
             public char Id { get; set; }
 
-            public int MetadataSize { get; set; }
-
             public int MetadataSum { get; set; }
-
-            public int Children { get; set; }
 
             public List<int> MetadataValues { get; set; } = new List<int>();
 
@@ -112,7 +119,7 @@ namespace AdventOfCode
             /// <returns>A string that represents the current object.</returns>
             public override string ToString()
             {
-                return $"{this.Id}, Children: {this.Children}, Metadata: {this.MetadataSize}, Current Sum: {this.MetadataSum}";
+                return $"{this.Id}, Children: {this.ChildNodes.Count}, Metadata: {this.MetadataValues.Count}, Current Sum: {this.MetadataSum}";
             }
         }
     }
